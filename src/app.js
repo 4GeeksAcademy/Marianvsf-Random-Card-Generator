70d1209211ebaf941d490fc3d0ac92f22ad38409
Marianvsf <marianvsf@gmail.com>
2024-09-09 20:10:53 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let icon1 = document.querySelector("#icon1");
+  let bothIcons = (icon1.innerHTML = randomIcon());
+  let icon = document.querySelector("#icon2");
+  icon.innerHTML = bothIcons;
+  let number1 = document.querySelector("#myNumber");
+  number1.innerHTML = randomNumber();
 };
+function randomNumber() {
+  let number1 = Math.floor(Math.random() * 13) + 1;
+  if (number1 === 11) {
+    return "J";
+  } else if (number1 === 12) {
+    return "Q";
+  } else if (number1 === 13) {
+    return "K";
+  } else if (number1 === 1) {
+    return "A";
+  }
+  let number2 = number1;
+  return number2;
+}
+
+function randomIcon() {
+  let icons = Math.floor(Math.random() * 4);
+  let arricons = ["♠", "♥", "♦", "♣"];
+  if (icons === 1 || icons === 2) {
+    document.getElementById("icon1").classList.add("red");
+    document.getElementById("icon2").classList.add("red");
+  }
+
+  icons = arricons[icons];
+  return icons;
+}
